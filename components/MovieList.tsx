736cd46d209dac53d39134e65f7b769b6e796eae
Carlos Garcia <ca.garcia2011@gmail.com>
2023-04-01 02:14:59 -0400

    Add my list page

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { MovieInterface } from '@/types';
 import { MovieCard, MovieSlider } from '.';
@@ -11,17 +11,36 @@ interface MovieListProps {
 }
 
 export const MovieList: React.FC<MovieListProps> = ({ data, title, isSlider }) => {
+    const [width, setWidth] = useState(window.innerWidth)
+    
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
     if (isEmpty(data)) {
         return null;
     }
+
 
     return (
         <div className="px-4 md:px-12 mt-4 space-y-8 w-full relative flex items-center justify-center">
             <div className='w-full relative overflow-visible movie-slider-container'>
                 <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4 z-0">{title}</p>
                 {
-                    isSlider
-                    ?
+                    isSlider &&
+                        (
+                            (width >= 1025 && data.length >= 5)
+                            || (width < 1025 && width >= 768 && data.length >= 3)
+                            || (width < 768 && data.length >= 2)
+                        )
+                        ?
                         <MovieSlider numberOfMovies={data.length} >
                             {
                                 data.map((movie) => (
@@ -29,12 +48,12 @@ export const MovieList: React.FC<MovieListProps> = ({ data, title, isSlider }) =
                                 ))
                             }
                         </MovieSlider>
-                    :
-                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
-                        {data.map((movie) => (
-                            <MovieCard key={movie.id} data={movie} />
-                        ))}
-                    </div>
+                        :
+                        <div className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2`}>
+                            {data.map((movie) => (
+                                <MovieCard key={movie.id} data={movie} />
+                            ))}
+                        </div>
                 }
             </div>
         </div>
--- pages/myList.tsx
+++ pages/myList.tsx
@@ -1,7 +1,30 @@
+import { MovieList } from "@/components"
+import { useFavorites } from "@/hooks"
+import Head from "next/head"
+import { AiOutlineLoading } from "react-icons/ai"
 
 
 const MyList = () => {
-    return <div className="text-white">My List</div>
+    const { data: favorites = [], error, isLoading } = useFavorites()
+
+    return <>
+        <Head>
+            <title>CGFlix Clone My List</title>
+        </Head>
+        {
+            isLoading
+                ?
+                <div className='w-full h-[100svh] flex items-center justify-center relative z-50'>
+                    <AiOutlineLoading size={60} color='#FFF' className='animate-spin' />
+                </div>
+                :
+                <>
+                    <div className="pb-10 w-full absolute top-[5rem]">
+                        <MovieList isSlider={false} title="My List" data={favorites} />
+                    </div>
+                </>
+        }
+    </>
 }
 
 export default MyList
